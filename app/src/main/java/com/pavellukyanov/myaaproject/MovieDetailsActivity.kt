afb79b455718d31106c50504149beed14989582c
lukyanovpavel <pavel.lukyanovv@gmail.com>
2020-11-17 16:37:50 +0300

    Исправление без лишних вью и констрейнтов, привязка к ориентации экрана -- добавлен RatingBar, но не настроен правильно

--- app/src/main/java/com/pavellukyanov/myaaproject/MovieDetailsActivity.kt
+++ app/src/main/java/com/pavellukyanov/myaaproject/MovieDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.pavellukyanov.myaaproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar
 
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
