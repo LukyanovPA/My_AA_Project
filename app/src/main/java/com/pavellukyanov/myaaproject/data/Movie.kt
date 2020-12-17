899bd47bcd449f9848ac0c025781f574fab6b140
lukyanovpavel <pavel.lukyanovv@gmail.com>
2020-12-17 21:47:38 +0300

    Домашка #5 done

--- app/src/main/java/com/pavellukyanov/myaaproject/data/Movie.kt
+++ app/src/main/java/com/pavellukyanov/myaaproject/data/Movie.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Movie(
+class Movie(
     val id: Int,
     val title: String,
     val overview: String,
